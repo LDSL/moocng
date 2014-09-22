b4495feba37795be52c497928758ea9ec80fc421
root <root@ip-172-31-34-4.eu-west-1.compute.internal>
2014-09-22 09:00:25 +0000

    Fix bug in apps.py

--- moocng/externalapps/apps.py
+++ moocng/externalapps/apps.py
@@ -11,7 +11,7 @@ class Askbot(ExternalApp):
         instance_type = 'askbot'
         instances = settings.MOOCNG_EXTERNALAPPS['askbot']['instances']
 
-class Nodebb(ExternalApp):
+class NodeBB(ExternalApp):
 
     class Meta:
         model = 'externalapps.ExternalApp'
