4b87d5da5cfb757b2b9c8bbf31798f5f1f96310b
Raúl Yeguas <neokore@gmail.com>
2014-11-06 22:04:08 +0100

    Improved activity and fixes

--- moocng/api/resources.py
+++ moocng/api/resources.py
@@ -879,6 +879,9 @@ class ActivityResource(BaseMongoUserResource):
     course_id = fields.IntegerField(null=False)
     unit_id = fields.IntegerField(null=False)
     kq_id = fields.IntegerField(null=False)
+    timestamp = fields.IntegerField(null=False)
+    lat = fields.FloatField()
+    lon = fields.FloatField()
 
     class Meta:
         resource_name = 'activity'
@@ -897,7 +900,10 @@ class ActivityResource(BaseMongoUserResource):
             "course_id": 1,
             "unit_id": 1,
             "kq_id": 1,
-            "user_id": 1
+            "user_id": 1,
+            "timestamp": 1,
+            "lat": 1.1,
+            "lon": 1.1
         }
 
     def _initial(self, request, **kwargs):
@@ -907,6 +913,9 @@ class ActivityResource(BaseMongoUserResource):
             "unit_id": -1,
             "kq_id": -1,
             "user_id": -1,
+            "timestamp": -1,
+            "lat": 0.0,
+            "lon": 0.0
         }
 
 
