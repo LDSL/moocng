0eefccaf32c02c388a3f1cc3bcfb846bddfe84bc
Raúl Yeguas <neokore@gmail.com>
2015-06-24 10:46:00 +0200

    Created utility to export history to xAPI

---
+++ moocng/courses/management/commands/export_xapi_history.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+
+import requests
+import sys
+import json
+import re
+import time
+import calendar
+from optparse import make_option
+
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand, CommandError
+
+from moocng.users.models import User
+from moocng.courses.models import Course
+from moocng.x_api.utils import learnerAccessAPage, learnerEnrollsInMooc
+from moocng import mongodb
+
+class Command(BaseCommand):
+	option_list = BaseCommand.option_list + (
+		make_option('-d', '--date',
+					action='store',
+					dest='date',
+					default='',
+					help='Max date to export. Format "DD/MM/YYYY hh:mm"'),
+	)
+
+	def error(self, message):
+		self.stderr.write("%s\n" % message.encode("ascii", "replace"))
+
+	def message(self, message):
+		self.stdout.write("%s\n" % message.encode("ascii", "replace"))
+
+	def handle(self, *args, **options):
+		if options['date']:
+			max_timestamp = calendar.timegm(time.strptime(options['date'], '%d/%m/%Y %H:%M')) * 1000
+		else:
+			max_timestamp = calendar.timegm(time.gmtime()) * 1000
+
+		# Get history
+		history_col = mongodb.get_db().get_collection('history')
+		histories = history_col.find({'timestamp': {'$lte': max_timestamp}})
+		
+		# Send each history entry as xAPI Statement
+		print "%d histories until %d" % (histories.count(), max_timestamp)
+		for history in histories:
+			course = None
+			try:
+				user_id = str(int(history['user_id']))
+				user = User.objects.get(pk=user_id)
+				if 'course_id' in history:
+					course_id = str(int(history['course_id']))
+					course = Course.objects.get(pk=course_id)
+			except (ValueError, TypeError):
+				continue
+			
+			page = {}
+			page['url'] = history['url']
+			if course:
+				page['name'] = course.name
+				page['description'] = course.name
+			else:
+				page['name'] = ''
+				page['description'] = ''
+			
+			geolocation = {
+				'lat': history['lat'],
+				'lon': history['lon']
+			}
+			learnerAccessAPage(user, page, geolocation)
